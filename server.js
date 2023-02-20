5374238d131062b610ac28550f7c9ab49ecd4a79
rekhan <rekhan@expediagroup.com>
2023-02-20 11:18:07 +0000

    finished with activity

--- server.js
+++ server.js
@@ -1,1 +1,9 @@
+const express = require("express");
+const app = express();
 
+const PORT = 3000;
+app.use(express.static("public"));
+
+app.listen(PORT, () => {
+  console.log(`Running Express on Server Port:${PORT}`);
+});
